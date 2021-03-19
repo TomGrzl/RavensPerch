c4de4badf65187b98f45bd3d2034a3c46e6eceac
Tom Grehl <grehl.tom@googlemail.com>
2021-03-19 17:14:10 +0100

    Added event handler to track and set coordinates of object in order to prepare saving and loading of states

--- frontend/src/pages/Konva.js
+++ frontend/src/pages/Konva.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Stage, Layer, Rect, Circle} from 'react-konva';
 
 export default function Konva() {
@@ -7,18 +7,17 @@ export default function Konva() {
     const [rectId, setRectId] = useState(1)
     const [circleId, setCircleId] = useState(1)
 
-
     const addRect = () => {
         setRects([...rects, {
             id: rectId.toString(),
             x: 60,
             y: 60,
-            width: 60,
-            height: 60,
+            width: 30,
+            height: 30,
             fill: '#C9E1F2',
             stroke: '#000',
             strokeWidth: 1,
-            shadowBlur: 10
+            shadowBlur: 10,
         }
         ])
         setRectId(rectId + 1);
@@ -29,8 +28,8 @@ export default function Konva() {
             id: circleId.toString(),
             x: 60,
             y: 60,
-            radius: 30,
-            fill: '#c9e1f2',
+            radius: 15,
+            fill: '#C9E1F2',
             stroke: '#000',
             strokeWidth: 1,
             shadowBlur: 10
@@ -39,10 +38,85 @@ export default function Konva() {
         setCircleId(circleId + 1);
     }
 
+
+    const handleDragStart = (e) => {
+        const id = e.target.id();
+        const x = e.target.x();
+        const y = e.target.y();
+
+        console.log(
+            'ID: ' + id + '  ' +
+            'X: ' + x + '  ' +
+            'Y: ' + y)
+    }
+
+    const setRectCoordinatesOnDragEnd = (e) => {
+        const id = e.target.id();
+        const x = e.target.x();
+        const y = e.target.y();
+
+        setRects(
+            rects.map((rect) => {
+                if (rect.id === id) {
+                    return {
+                        ...rect,
+                        x: x,
+                        y: y,
+                        isDragging: false,
+                        width: 30,
+                        height: 30,
+                        shadowBlur: 10,
+                        fill: '#C9E1F2', shadowOffsetX: 5, shadowOffsetY: 5
+                    }
+                } else {
+                    return {...rect}
+                }
+            }))
+
+        console.log(
+            'ID: ' + id + '  ' +
+            'X: ' + x + '  ' +
+            'Y: ' + y)
+    }
+
+    const setCircleCoordinatesOnDragEnd = (e) => {
+        const id = e.target.id();
+        const x = e.target.x();
+        const y = e.target.y();
+
+        setCircles(
+            circles.map((circle) => {
+                if (circle.id === id) {
+                    return {
+                        ...circle,
+                        x: x,
+                        y: y,
+                    }
+                } else {
+                    return {...circle}
+                }
+            }))
+        
+        console.log(
+            'ID: ' + id + '  ' +
+            'X: ' + x + '  ' +
+            'Y: ' + y)
+    }
+
+    const logRects = () => {
+        console.log(rects)
+    }
+
+    const logCircles = () => {
+        console.log(circles)
+    }
+
     return (
         <>
             <button onClick={addRect}>Add Rect</button>
             <button onClick={addCircle}>Add Circle</button>
+            <button onClick={logRects}>Log Rects</button>
+            <button onClick={logCircles}>Log Circles</button>
             <Stage width={window.innerWidth} height={window.innerHeight} background={'#fff'}>
                 <Layer>
                     {rects.map((rect) => (
@@ -58,6 +132,9 @@ export default function Konva() {
                             strokeWidth={rect.strokeWidth}
                             shadowBlur={rect.shadowBlur}
                             draggable
+                            isDragging={false}
+                            onDragStart={handleDragStart}
+                            onDragEnd={setRectCoordinatesOnDragEnd}
                         />))}
                     {circles.map((circle) => (
                         <Circle
@@ -71,6 +148,8 @@ export default function Konva() {
                             strokeWidth={circle.strokeWidth}
                             shadowBlur={circle.shadowBlur}
                             draggable
+                            onDragStart={handleDragStart}
+                            onDragEnd={setCircleCoordinatesOnDragEnd}
                         />))}
                 </Layer>
             </Stage>
