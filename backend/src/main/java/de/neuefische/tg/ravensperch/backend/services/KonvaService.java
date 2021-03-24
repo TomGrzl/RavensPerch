0c54d1158e0044ea101a8054423a3f6f48afb34a
Tom Grehl <grehl.tom@googlemail.com>
2021-03-24 12:34:26 +0100

    added access identifier for konvaDB in konvaService
    Co-authored-by: Christopher Siem <65181238+christophersiem@users.noreply.github.com>

--- backend/src/main/java/de/neuefische/tg/ravensperch/backend/services/KonvaService.java
+++ backend/src/main/java/de/neuefische/tg/ravensperch/backend/services/KonvaService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Service
 public class KonvaService {
 
-    KonvaDB konvaDB;
+    private final KonvaDB konvaDB;
 
     @Autowired
     private KonvaService(KonvaDB konvaDB) {
