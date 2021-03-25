884d9fcfea8b55a5d718371f922ef56f940abc89
Tom Grehl <grehl.tom@googlemail.com>
2021-03-25 11:11:15 +0100

    Added test classes for KonvaController and KonvaService

---
+++ backend/src/test/java/de/neuefische/tg/ravensperch/backend/controller/KonvaControllerTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.tg.ravensperch.backend.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KonvaControllerTest {
+
+}
---
+++ backend/src/test/java/de/neuefische/tg/ravensperch/backend/services/KonvaServiceTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.tg.ravensperch.backend.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KonvaServiceTest {
+
+}
